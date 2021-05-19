780c0a5909a89f1c659e50bccaf465ff71c3094e
Ishmael-sikhikhi <i.sikhikhi@gmail.com>
2021-05-19 16:22:52 +0200

    redeploy

---
+++ test-unit/registration.test.js
@@ -0,0 +1,5 @@
+describe('Registration number exercise', ()=>{
+    it('Should take registration number that starts with 2 or more alphabets and first letter should be C', ()=>{
+        
+    })
+})
